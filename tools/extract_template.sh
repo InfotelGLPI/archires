7bea2df4f6c6ab72298408f395aad97a49221eb4
Thetsmr <thetsmr@gmail.com>
2026-08-26 11:21:16 +0200

    Fix CI

--- tools/extract_template.sh
+++ tools/extract_template.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
+#
+# -------------------------------------------------------------------------
+# archires plugin for GLPI
+# -------------------------------------------------------------------------
+#
+# LICENSE
+#
+# This file is part of archires.
+#
+# archires is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# archires is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with archires. If not, see <http://www.gnu.org/licenses/>.
+# -------------------------------------------------------------------------
+# @copyright Copyright (C) 2009-2026 by archires plugin team.
+# @license   AGPLv3 https://www.gnu.org/licenses/agpl-3.0.html
+# @link      https://github.com/InfotelGLPI/archires
+# --------------------------------------------------------------------------
+#
+
 # Only strings with domain specified are extracted (use Xt args of keyword param to set number of args needed)
 
 xgettext *.php */*.php --copyright-holder='Archires Development Team' --package-name='GLPI - Archires plugin' --package-version='1.0.0' -o locales/glpi.pot -L PHP --add-comments=TRANS --from-code=UTF-8 --force-po  \
