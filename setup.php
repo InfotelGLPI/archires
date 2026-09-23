43112903aaf14dbb65af533fb7f203ec10ff7d19
Thetsmr <thetsmr@gmail.com>
2026-09-23 17:08:38 +0200

    Bump version

--- setup.php
+++ setup.php
@@ -32,7 +32,7 @@ use Glpi\Plugin\Hooks;
 use GlpiPlugin\Archires\Archires;
 use GlpiPlugin\Archires\Profile;
 
-define('PLUGIN_ARCHIRES_VERSION', '1.1.5');
+define('PLUGIN_ARCHIRES_VERSION', '1.1.6');
 
 global $CFG_GLPI;
 
